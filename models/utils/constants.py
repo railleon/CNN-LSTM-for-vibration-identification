2232ef6ff4da0221af3c43e3e84a22d7e3d3fefb
Ruihua Liang <103068333+railleon@users.noreply.github.com>
2022-07-29 17:00:27 +0100

    Add files via upload

---
+++ models/utils/constants.py
@@ -0,0 +1,23 @@
+
+TRAIN_FILES = ['../data/train/',  # 49
+               '../data/validation/',  # 50
+
+               ]
+
+TEST_FILES = ['../data/train/',  # 49
+              '../data/validation/',  # 50
+              ]
+
+MAX_NB_VARIABLES = [64,  # 49
+                    64,  # 50
+                    ]
+
+MAX_TIMESTEPS_LIST = [64,  # 49
+                      64,  # 50
+                      ]
+
+
+NB_CLASSES_LIST = [2,  # 49
+                   2,  # 50
+
+                   ]
